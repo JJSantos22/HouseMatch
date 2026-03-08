f59da17745dcbf20fdf82f07c38abbfc1933cd77
andrevcaseiro <93094058+andrevcaseiro@users.noreply.github.com>
2026-03-08 20:18:06 +0000

    feat: add login/signin page layouts

---
+++ frontend/app/(entry)/login/page.tsx
@@ -0,0 +1,117 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+import EntryLayout from "../_components/EntryLayout";
+
+export default function LoginPage() {
+  const universities = [
+    { name: "Universidade de Lisboa", city: "Lisboa" },
+    { name: "Politécnico de Lisboa", city: "Lisboa" },
+    { name: "Universidade Nova de Lisboa", city: "Lisboa" },
+    { name: "Universidade do Porto", city: "Porto" },
+  ];
+
+  const universitiesByCity = universities.reduce(
+    (acc, u) => {
+      if (!acc[u.city]) acc[u.city] = [];
+      acc[u.city].push(u);
+      return acc;
+    },
+    {} as Record<string, typeof universities>,
+  );
+
+  return (
+    <EntryLayout>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Login</CardTitle>
+          <CardDescription>Login through your university</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="university">University</FieldLabel>
+                <Select>
+                  <SelectTrigger className="w-45">
+                    <SelectValue placeholder="University" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Object.entries(universitiesByCity).map(
+                      ([city, universities]) => (
+                        <SelectGroup key={city}>
+                          <SelectLabel>{city}</SelectLabel>
+                          {universities.map((u) => (
+                            <SelectItem key={u.name} value={u.name}>
+                              {u.name}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      ),
+                    )}
+                  </SelectContent>
+                </Select>
+              </Field>
+              <FieldSeparator>or login as landlord</FieldSeparator>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account?{" "}
+                  <Link href="/signup">Sign up</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center text-primary-foreground">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </EntryLayout>
+  );
+}
---
+++ frontend/app/(entry)/signup/page.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import EntryLayout from "../_components/EntryLayout";
+
+export default function LoginPage() {
+  return (
+    <EntryLayout>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Signup</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Repeat Password</FieldLabel>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Signup</Button>
+                <FieldDescription className="text-center">
+                  Already have an account? <Link href="/login">Log in</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center text-primary-foreground">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </EntryLayout>
+  );
+}
